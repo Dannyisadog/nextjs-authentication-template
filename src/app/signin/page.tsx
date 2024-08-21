ba16c689a1ad03c35f3e4c55a6f8048f69fc53cd
Danny Chen <dannychen@kkcompany.com>
2024-08-21 23:04:55 +0800

    feat(page): add signin page

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,20 @@
+import { auth } from "@/auth";
+import styles from "../page.module.css";
+import GoogleSigninButton from "@/components/client/GoogleSigninButton";
+import { SigninForm } from "@/components/client/SigninForm";
+import { redirect } from "next/navigation";
+
+export default async function Signin() {
+  const session = await auth();
+
+  if (session) {
+    redirect("/");
+  }
+
+  return (
+    <div className={styles.container}>
+      <SigninForm />
+      <GoogleSigninButton />
+    </div>
+  );
+}
