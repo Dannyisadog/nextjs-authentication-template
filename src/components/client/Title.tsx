72e48184d7f9d99f0ef3d6ea0d0cffad289938ce
DannyChen <dannychen@kkcompany.com>
2024-08-23 13:34:31 +0800

    feat(component): add goBack option in title component

--- src/components/client/Title.tsx
+++ src/components/client/Title.tsx
@@ -1,14 +1,24 @@
 import { Stack, Typography } from "@mui/material";
+import Link from "next/link";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 
 interface TitleProps {
   text: string;
+  hasGoBack?: boolean;
 }
 
 export default function Title(props: TitleProps) {
-  const { text } = props;
+  const { text, hasGoBack } = props;
   return (
     <Stack height={100} justifyContent="center">
-      <Typography variant="h4">{text}</Typography>
+      <Stack spacing={2}>
+        {hasGoBack && (
+          <Link href="/signin">
+            <ArrowBackIosNewIcon />
+          </Link>
+        )}
+        <Typography variant="h4">{text}</Typography>
+      </Stack>
     </Stack>
   );
 }
