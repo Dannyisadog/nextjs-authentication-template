232068a92085dd1e875377cd148463c682bd0845
DannyChen <dannychen@kkcompany.com>
2024-08-23 11:48:03 +0800

    feat(component): add loading status in google signin & signout button

--- src/components/client/GoogleSigninButton.tsx
+++ src/components/client/GoogleSigninButton.tsx
@@ -2,12 +2,27 @@
 
 import { signIn } from "next-auth/react";
 import Button from "./Button";
-import { Typography } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
+import GoogleIcon from "@mui/icons-material/Google";
+import { useState } from "react";
 
 export default function GoogleSigninButton() {
+  const [loading, setLoading] = useState(false);
+
   return (
-    <Button fullWidth variant="outlined" onClick={() => signIn("google")}>
-      <Typography>Sign in with Google</Typography>
+    <Button
+      isLoading={loading}
+      fullWidth
+      variant="outlined"
+      onClick={async () => {
+        setLoading(true);
+        await signIn("google");
+      }}
+    >
+      <Stack direction="row" spacing={1}>
+        <GoogleIcon />
+        <Typography>Sign in with Google</Typography>
+      </Stack>
     </Button>
   );
 }
