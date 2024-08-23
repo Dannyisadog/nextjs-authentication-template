770a450e40b19f4f64f579eac1792c93bc077be3
DannyChen <dannychen@kkcompany.com>
2024-08-23 11:22:31 +0800

    refactor(api): refactor user api using user repository

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -1,65 +1,13 @@
-import { PrismaClient } from "@prisma/client";
+import { remove } from "app/repository/user";
 import { NextRequest, NextResponse } from "next/server";
 
-const prisma = new PrismaClient();
-
-export async function PUT(request: NextRequest, {params}: {params: {id: string}}) {
-  const { id } = params;
-  const { name } = await request.json();
-
-  const user = await prisma.user.findFirst({
-    where: {
-      id: Number(id),
-    },
-  });
-
-  if (!user) {
-    return NextResponse.json(
-      { error: "User not found" },
-      {
-        status: 404
-      }
-    );
-  }
-
-  await prisma.user.update({
-    where: {
-      id: Number(id),
-    },
-    data: {
-      name,
-    },
-  });
-
-  return NextResponse.json({ message: 'Update user', });
-}
-
-export async function DELETE(request: NextRequest, {params}: {params: {id: string}}) {
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
   const { id } = params;
 
-  const user = await prisma.user.findFirst({
-    where: {
-      id: Number(id),
-    },
-  });
+  await remove(parseInt(id));
 
-  if (!user) {
-    return NextResponse.json(
-      { error: "User not found" },
-      {
-        status: 404
-      }
-    );
-  }
-
-  await prisma.user.update({
-    where: {
-      id: Number(id),
-    },
-    data: {
-      is_deleted: true,
-    },
-  });
-
-  return NextResponse.json({ message: 'Delete user', });
+  return NextResponse.json({ message: "Delete user" });
 }
