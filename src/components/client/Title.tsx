2c797d5965360984a9cf76eb9cfddde4ee0f13bc
Danny Chen <dannychen@kkcompany.com>
2024-08-23 23:41:49 +0800

    feat(component): display github icon conditionally

--- src/components/client/Title.tsx
+++ src/components/client/Title.tsx
@@ -6,12 +6,13 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 interface TitleProps {
   text: string;
   hasGoBack?: boolean;
+  showGithub?: boolean;
 }
 
 export default function Title(props: TitleProps) {
-  const { text, hasGoBack } = props;
+  const { text, hasGoBack, showGithub = true } = props;
   return (
-    <Stack height={130} justifyContent="center">
+    <Stack justifyContent="center">
       <Stack spacing={2}>
         <Stack direction="row" justifyContent="space-between">
           {hasGoBack ? (
@@ -21,12 +22,14 @@ export default function Title(props: TitleProps) {
           ) : (
             <Box />
           )}
-          <Link
-            href="https://github.com/Dannyisadog/nextjs-authjs-template"
-            target="_blank"
-          >
-            <GitHubIcon />
-          </Link>
+          {showGithub && (
+            <Link
+              href="https://github.com/Dannyisadog/nextjs-authjs-template"
+              target="_blank"
+            >
+              <GitHubIcon />
+            </Link>
+          )}
         </Stack>
         <Typography variant="h4">{text}</Typography>
       </Stack>
