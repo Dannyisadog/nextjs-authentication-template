0a05164db84cd63f9b92bc12089e1f0a1cdf8d7b
Danny Chen <dannychen@kkcompany.com>
2024-08-27 22:10:31 +0800

    feat(api): protect get users api

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,7 +1,12 @@
-import { NextRequest, NextResponse } from "next/server";
-import { create, list } from "app/repository/user";
+import { NextResponse } from "next/server";
+import { list } from "app/repository/user";
+import { auth } from "auth";
 
-export async function GET() {
-  const users = await list();
-  return NextResponse.json(users);
-}
+export const GET = auth(async (req) => {
+  if (req.auth) {
+    const users = await list();
+    return NextResponse.json(users);
+  }
+
+  return Response.json({ message: "Not authenticated" }, { status: 401 });
+});
