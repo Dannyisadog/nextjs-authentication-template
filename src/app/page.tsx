9961cb89c5fd40ea2cc5790bec5e41b97283639e
DannyChen <dannychen@kkcompany.com>
2024-08-29 15:28:21 +0800

    feat(component): split user avatar to component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,36 +4,20 @@ import { redirect } from "next/navigation";
 import { Box, Stack, Typography } from "@mui/material";
 import Title from "components/client/Title";
 import UserList from "components/client/UserList";
-import multiavatar from "@multiavatar/multiavatar/esm";
 import Image from "next/image";
+import UserAvatar from "components/client/UserAvatar";
 
 export default async function Home() {
   const session = await auth();
 
-  if (!session || !session.user?.email) {
+  if (!session || !session.user || !session.user?.email) {
     redirect("/signin");
   }
-
-  const svgCode = multiavatar(session.user.email);
 
   return (
     <>
       <Title text="Basic Info" />
-      {!session.user.image && (
-        <Box
-          dangerouslySetInnerHTML={{ __html: svgCode }}
-          width={64}
-          height={64}
-        />
-      )}
-      {session.user.image && (
-        <Image
-          src={session.user.image}
-          width={64}
-          height={64}
-          alt={`Avatar for ${session.user.name}`}
-        />
-      )}
+      <UserAvatar user={session.user} />
       <Stack direction="row" spacing={2}>
         <Typography textAlign="center">Account:</Typography>
         <Typography textAlign="center">{session.user?.email}</Typography>
