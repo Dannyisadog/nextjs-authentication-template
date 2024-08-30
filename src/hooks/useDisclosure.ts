3d373e9d4e155656f6f96a794c80715945298933
Danny Chen <dannyisadog10@gmail.com>
2024-08-30 15:31:42 +0800

    feat(hook): add useDisclosure hook

---
+++ src/hooks/useDisclosure.ts
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+export const useDisclosure = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onOpen = () => setIsOpen(true);
+  const onClose = () => setIsOpen(false);
+  const onToggle = () => setIsOpen(!isOpen);
+
+  return {
+    isOpen,
+    onOpen,
+    onClose,
+    onToggle,
+  };
+};
