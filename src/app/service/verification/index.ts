ebe99c22a7ea4067bd177da392936baac5783e94
DannyChen <dannychen@kkcompany.com>
2024-09-02 17:48:51 +0800

    feat(service): add verification service

---
+++ src/app/service/verification/index.ts
@@ -0,0 +1,33 @@
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET;
+const EXP = Math.floor(Date.now() / 1000) + 60 * 60;
+
+export interface VerifyToken {
+  data: {
+    email: string;
+  };
+  exp: number;
+}
+
+export const generateVerificationToken = (email: string): string => {
+  const token = jwt.sign(
+    {
+      exp: EXP,
+      data: {
+        email,
+      },
+    },
+    JWT_SECRET as string
+  );
+  return token;
+};
+
+export const verifyToken = (token: string): VerifyToken | null => {
+  try {
+    return jwt.verify(token, JWT_SECRET as string) as VerifyToken;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
