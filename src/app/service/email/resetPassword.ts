1c663668302ebd7d5c61dc9ee1784c47a3d9fb36
DannyChen <dannychen@kkcompany.com>
2024-09-03 12:07:24 +0800

    feat(service): add service to send reset password email

---
+++ src/app/service/email/resetPassword.ts
@@ -0,0 +1,21 @@
+import ResetPasswordEmail from "components/email/ResetPassword";
+import { sendEmail } from ".";
+import { generateResetPasswordToken } from "../resetPassword";
+import { get } from "app/repository/user";
+
+export const sendResetPasswordEmail = async (email: string) => {
+  const subject = "Reset your password";
+
+  const resetPasswordToken = generateResetPasswordToken(email);
+
+  const user = await get({ email });
+
+  sendEmail({
+    to: [email],
+    subject,
+    template: ResetPasswordEmail({
+      firstName: user.name,
+      token: resetPasswordToken,
+    }),
+  });
+};
